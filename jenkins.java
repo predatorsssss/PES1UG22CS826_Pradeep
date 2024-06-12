d2c86bf023b37dc594702cd73c1fa4b059382a59
pradeep kumar <156198386+predatorsssss@users.noreply.github.com>
2024-06-12 12:00:04 +0530

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -2,5 +2,7 @@
 public class jenkins {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
+        System.out.println("Hello, World!");
     }
 }
