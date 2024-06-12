7b287688c0a18cd615c4eef0767fb29d2b83db94
pradeep kumar <156198386+predatorsssss@users.noreply.github.com>
2024-06-12 12:26:14 +0530

    Update jenkins.java

--- jenkins.java
+++ jenkins.java
@@ -2,5 +2,6 @@
 public class jenkins {
     public static void main(String[] args) {
         System.out.println("hello everyone welcome to jenkins");
+         System.out.println("i am pradeep kumar");
     }
 }
